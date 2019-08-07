94cc613a500a33fe7aa511f1b4312e2a0cfe675b
Daniel Kovalenko <animtel@gmail.com>
2019-08-07 16:20:38 +0300

    Updates

--- precache-manifest.b843d0575b64f152c1080c7217ada6c2.js
+++ precache-manifest.b843d0575b64f152c1080c7217ada6c2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "feb34ac9f071258987431cfd0caa3062",
+    "revision": "f637d7d0d8be9a750b74ac21aff0cbc4",
     "url": "/ReactGrowApp/index.html"
   },
   {
-    "revision": "9ffe51ffa9db04b6c173",
+    "revision": "062f2090aad5ed0c3d02",
     "url": "/ReactGrowApp/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "9d17369bc1464cc4843c",
+    "revision": "a6ba2025931cc156f50f",
     "url": "/ReactGrowApp/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "9ffe51ffa9db04b6c173",
-    "url": "/ReactGrowApp/static/js/2.c5fa857b.chunk.js"
+    "revision": "062f2090aad5ed0c3d02",
+    "url": "/ReactGrowApp/static/js/2.bcc2bc8d.chunk.js"
   },
   {
-    "revision": "9d17369bc1464cc4843c",
-    "url": "/ReactGrowApp/static/js/main.18e73a04.chunk.js"
+    "revision": "a6ba2025931cc156f50f",
+    "url": "/ReactGrowApp/static/js/main.7b2f0e9e.chunk.js"
   },
   {
     "revision": "b44d14c2fd23f11f9526",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactGrowApp/precache-manifest.cf5cc6d309236ec0bfe75ded62517068.js"
+  "/ReactGrowApp/precache-manifest.b843d0575b64f152c1080c7217ada6c2.js"
 );
 
 self.addEventListener('message', (event) => {
