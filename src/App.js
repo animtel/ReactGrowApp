e4c268e142b7a53719c86c358a8e1d9c2b1f3148
Daniel Kovalenko <animtel@gmail.com>
2019-08-07 15:44:57 +0300

    [added] - update contacts;

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "./context";
 
@@ -21,6 +22,7 @@ class App extends React.Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 {/* for passing params to route, use :paramName and in component you can call props.match.prams.paramName */}
                 <Route exact path="/about" component={About} />
                 <Route component={NotFound} />
